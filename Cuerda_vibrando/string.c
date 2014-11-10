d8c4ebc8ead096870f7c03d2395e304403b6a75a
auracuervog <aura.cuervo.g10@gmail.com>
2014-11-10 17:55:14 -0500

    cambios

--- Cuerda_vibrando/string.c
+++ Cuerda_vibrando/string.c
@@ -3,9 +3,9 @@
 #include <math.h>
 #define T 40 //N
 #define L 100 //M
-#define N 100
+#define N 101
 
-float *copy(float *u);
+void copy(float *u, float *u_new);
 
 int main(int argc, char **argv)
 {
@@ -18,12 +18,13 @@ int main(int argc, char **argv)
   float delta_x, delta_t;
   int i, j,t;
 
-  npoints = 100.0;
+  npoints = 10.0;
   rho = atof(argv[1]);
-  delta_x = (L/npoints);
-  delta_t = 0.0005;
+  delta_x = (L/(npoints-1));
+  delta_t = 0.1;
   c = sqrt(T/rho);
   r = c*(delta_t/delta_x);
+  printf("%f\n",r);
 
   u_initial = malloc(sizeof(float)*N);
   u_future = malloc(sizeof(float)*N);
@@ -45,7 +46,7 @@ int main(int argc, char **argv)
     if((i*delta_x) <= (0.8*L)){
       u_initial[i] = (1.25*i*delta_x)/L;
     }
-    else{
+    else if((i*delta_x)>(0.8*L)){
       u_initial[i] = (5-((5*i*delta_x)/L));
     }
   }
@@ -57,59 +58,55 @@ int main(int argc, char **argv)
   u_future[N-1] = 0.0;
 
   for(i=0;i<N;i++){
-    fprintf(file,"%f ",u_initial[i]);
+    fprintf(file,"%.20f ",u_initial[i]);
   }
   fprintf(file,"\n");
 
   //Primer paso
   for(i=0;i<N;i++){
     if(i==0){
-      fprintf(file,"%f ",u_future[0]);
+      fprintf(file,"%.20f ",u_future[0]);
     }
     else if(i==N-1){
-      fprintf(file,"%f ",u_future[0]);
+      fprintf(file,"%.20f ",u_future[N-1]);
     }
     else{
       u_future[i] = u_initial[i] + (pow(r,2)/2.0) * (u_initial[i+1] - 2.0*u_initial[i] + u_initial[i-1]);
-      fprintf(file,"%f ",u_future[i]);
+      fprintf(file,"%.20f ",u_future[i]);
     } 
   }
   fprintf(file,"\n");
 
-  u_past = copy(u_initial);
-  u_present = copy(u_future);
+  // u_past = copy(u_initial);
+  copy(u_initial,u_past);
+  copy(u_future,u_present);
   
   //Iteraciones siguientes
-  t = 120;
+  t = 120/delta_t;
   for(j=1;j<t;j++){
     for(i=0;i<N;i++){
       if(i==0){
-	u_future[0] = 0.0;
-	fprintf(file,"%f ",u_future[0]);
+	fprintf(file,"%.20f ",u_future[0]);
       }
       else if(i==N-1){
-	u_future[N-1] = 0.0;
-	fprintf(file,"%f ",u_future[N-1]);
+	fprintf(file,"%.20f ",u_future[N-1]);
       }
       else{     
 	u_future[i] = ((2.0*(1.0-pow(r,2)))*u_present[i]) - u_past[i] + ((pow(r,2))*(u_present[i+1] + u_present[i-1]));
-	fprintf(file,"%f ",u_future[i]);
+	fprintf(file,"%.20f ",u_future[i]);
       }
     }
-    u_past = copy(u_present);
-    u_present = copy(u_future);
+    copy(u_present,u_past);
+    copy(u_future,u_present);
     fprintf(file,"\n");
   }
   return 0;
 }
 
-float *copy(float *u){
-  float *u_new;
+void copy(float *u, float *u_new){
   int i;
-  u_new = malloc(sizeof(float)*N);
-
+  
   for(i=0;i<N;i++){
     u_new[i] = u[i];
   }
-  return u_new;
 }
